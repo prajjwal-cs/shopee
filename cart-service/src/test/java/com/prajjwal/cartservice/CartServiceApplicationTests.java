2e7eb613398cfcf54d8f4e4b06a6510b55ff47bc
prajjwal <prajjwalpachauri725@gmail.com>
2026-04-01 14:40:14 +0530

    Initial commit

---
+++ cart-service/src/test/java/com/prajjwal/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prajjwal.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
