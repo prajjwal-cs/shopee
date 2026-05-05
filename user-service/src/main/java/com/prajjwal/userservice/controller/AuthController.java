9a5b5b3be5c0870cbe42833c963e79a130575a60
prajjwal <prajjwalpachauri725@gmail.com>
2026-05-05 12:21:45 +0530

    added api version

--- user-service/src/main/java/com/prajjwal/userservice/controller/AuthController.java
+++ user-service/src/main/java/com/prajjwal/userservice/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class AuthController {
 
