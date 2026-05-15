cd07f672b775d223ddb1ed16321b2d4ba45e661c
prajjwal <prajjwalpachauri725@gmail.com>
2026-05-15 17:26:22 +0530

    Implemented kafka configs

---
+++ order-service/src/main/java/com/prajjwal/orderservice/config/KafkaConsumerConfig.java
@@ -0,0 +1,47 @@
+package com.prajjwal.orderservice.config;
+
+
+import com.prajjwal.orderservice.event.PaymentResultEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.util.ConditionalOnBootstrapEnabled;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ConsumerFactory<String, PaymentResultEvent> consumerFactory() {
+        JsonDeserializer<PaymentResultEvent> deserializer =
+                new JsonDeserializer<>(PaymentResultEvent.class, false);
+        deserializer.addTrustedPackages("*");
+
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "order-service-group");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), deserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PaymentResultEvent> kafkaListenerContainerFactory() {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, PaymentResultEvent>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
---
+++ order-service/src/main/java/com/prajjwal/orderservice/config/KafkaTopicConfig.java
@@ -0,0 +1,34 @@
+package com.prajjwal.orderservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic orderPlacedTopic() {
+        return TopicBuilder.name("order.placed")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic orderCancelledTopic() {
+        return TopicBuilder.name("order.cancelled")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic orderStatusUpdatedTopic() {
+        return TopicBuilder.name("order.status.updated")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
